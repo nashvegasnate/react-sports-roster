6a7b0a152a7c346ade258ea17116b4ef7bbe0c72
Nathan Keith <nashvegasnate@gmail.com>
2021-05-11 12:46:36 -0500

    created EditPlayerForm to fix the oversized edit form

---
+++ src/components/EditPlayerForm.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from 'reactstrap';
+import PropTypes from 'prop-types';
+import { addPlayer, updatePlayer } from '../helpers/data/PlayerData';
+
+const EditPlayerForm = ({
+  formTitle,
+  setPlayers,
+  name,
+  position,
+  imageUrl,
+  firebaseKey,
+  uid
+}) => {
+  const [player, setPlayer] = useState({
+    name: name || '',
+    position: position || '',
+    imageUrl: imageUrl || '',
+    firebaseKey: firebaseKey || null,
+    uid: uid || null,
+  });
+  const history = useHistory();
+
+  const handleInputChange = (e) => {
+    setPlayer((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (player.firebaseKey) {
+      // make call to updateStudent to update student and rerender the DOM
+      updatePlayer(player).then(setPlayers);
+    } else {
+      // addPlayer(player).then(setPlayers);
+      // history.push('/players');
+      addPlayer(player).then((response) => {
+        setPlayers(response);
+        history.push('/players');
+      });
+
+      setPlayer({
+        name: '',
+        position: '',
+        imageUrl: '',
+        firebaseKey: null,
+        uid: null,
+      });
+    }
+  };
+
+  return (
+    <div className='edit-player-form'>
+      <Form id='EditPlayerForm' autoComplete='off' onSubmit={handleSubmit}>
+        <h2>{formTitle}</h2>
+        <FormGroup>
+          <Label for="name">Name:</Label>
+          <Input
+            name='name'
+            id='name'
+            value={player.name}
+            type='text'
+            placeholder='Enter a Player Name'
+            onChange={handleInputChange}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="position">Position:</Label>
+          <Input
+            name='position'
+            id='position'
+            value={player.position}
+            type='text'
+            placeholder='Enter Position'
+            onChange={handleInputChange}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="imageUrl">Image URL:</Label>
+          <Input
+            name='imageUrl'
+            id='imageUrl'
+            value={player.imageUrl}
+            type='string'
+            placeholder='Enter Image URL'
+            onChange={handleInputChange}
+          />
+        </FormGroup>
+
+        <Button type='submit'>Submit</Button>
+      </Form>
+    </div>
+  );
+};
+
+EditPlayerForm.propTypes = {
+  formTitle: PropTypes.string.isRequired,
+  setPlayers: PropTypes.func,
+  name: PropTypes.string,
+  position: PropTypes.string,
+  imageUrl: PropTypes.string,
+  firebaseKey: PropTypes.string,
+  uid: PropTypes.string
+};
+
+export default EditPlayerForm;
